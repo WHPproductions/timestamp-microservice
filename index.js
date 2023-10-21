3e6ec09cf2dbfc9ec6cc77d783388d3771053f38
WHPproductions <139813100+WHPproductions@users.noreply.github.com>
2023-10-22 00:56:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api", function (req, res) {
+  const time = new Date()
+  const UTCTimestamp = time.toUTCString()
+  const unixTimestamp = time.getTime()
+  res.json({
+    unix: unixTimestamp,
+    utc: UTCTimestamp,
+  })
+})
+
+app.get("/api/:time", function (req, res) {
+  const time = isNaN(req.params.time) 
+    ? new Date(req.params.time)
+    : new Date(parseInt(req.params.time, 10))
+  const UTCTimestamp = time.toUTCString()
+  const unixTimestamp = time.getTime()
+
+  if (UTCTimestamp == "Invalid Date") {
+    res.json({
+      error: 'Invalid Date',
+    });
+  } else {
+    res.json({
+      unix: unixTimestamp,
+      utc: UTCTimestamp,
+    });
+  }
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
